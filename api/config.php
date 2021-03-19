7b301d68dbceb7ef0b9a57602b3061c90ae08749
bektafa <bektafa@users.noreply.github.com>
2021-03-19 12:40:07 +0100

    Update config.php
    update config.php

--- api/config.php
+++ api/config.php
@@ -5,7 +5,7 @@ class Config {
     const DB_HOST = "localhost";
     const DB_USERNAME= "root";
     const DB_PASSWORD= "";
-    const DB_SCHEME = "sys";
+    const DB_SCHEME = "flight-reservation";
 }
 
 
