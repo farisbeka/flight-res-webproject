e89de731ed21e2f6d5fb4e5b431d6f60fe4d798f
bektafa <bektafa@users.noreply.github.com>
2021-03-21 18:51:46 +0100

    added some funcs

--- api/dao/AircraftsDao.class.php
+++ api/dao/AircraftsDao.class.php
@@ -1,3 +1,18 @@
 <?php
 
+require_once dirname(__FILE__)."/BaseDao.class.php";
+
+
+class AircraftsDao extends BaseDao{
+
+    public function return_aircraft_by_id($id)
+    {
+        return $this->query_unique("SELECT * FROM aircrafts WHERE id=:id", ["id" => $id]);
+    }
+
+    public function return_aircrafts_by_destination($destination)
+    {
+        return $this->query("SELECT * FROM aircrafts WHERE destination=:destination", ["destination" => $destination]);
+    }
+}
 ?>
--- api/dao/AirportsDao.class.php
+++ api/dao/AirportsDao.class.php
@@ -1,3 +1,20 @@
 <?php
 
+require_once dirname(__FILE__)."/../config.php";
+
+class AirportsDao extends BaseDao{
+
+
+    public function return_airport_by_id($id)
+    {
+        return $this->query_unique("SELECT * FROM airports WHERE id=:id", ["id" => $id]);
+    }
+
+    public function return_airport_by_city($city)
+    {
+        return $this->query_unique("SELECT * FROM airports WHERE airport_city=:city", ["city" => $city]);
+    }
+}
+
+
 ?>
